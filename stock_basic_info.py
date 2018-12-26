608fde8b0e82d776de78414e1b37a3c4dcc89d6c
summon0808 <401576290@qq.com>
2018-12-26 17:45:48 +0800

    init

---
+++ stock_basic_info.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+import time
+import json
+import math
+import datetime
+import sys
+import urllib
+import importlib
+
+import pandas
+import pymysql
+pymysql.install_as_MySQLdb()
+import MySQLdb
+importlib.reload(sys)
+sys.path.append('/home/shili.lzy/boom/chisong.xjm/lib')
+from MysqlLib import MysqlClass
+import matplotlib.pyplot as plt 
+
+class StockBasicInfo:
+    """获取股票基础信息类，包含换手率，流通股数，市盈率，收盘价，成交额
+    """
+
+    def __init__(self):
+        self.nmysql=MysqlClass('root5','Xjm@123456')
+        self.market_value_back_year = 5
+        self.market_value_blank_back_day = 365 * self.market_value_back_year
+        self.start_date=self.nmysql.getDate(- \
+                self.market_value_blank_back_day - 1)
+        self.end_date=self.nmysql.getDate(0)
+
+    def get_SHSZcode_order_by_market_value(self):
+        """返回深圳和上海所有股票的代码，按照市值降序
+        input: NULL
+        output: 如果失败返回-1，如果成功返回tuple股票代码序列
+        """
+        tmv=self.nmysql.Query('select sms.code,sms.update_time, \
+                sms.circular_market_val from stock_market_snapshot as \
+                sms join stock_uid_info as sui on \
+                sms.code=sui.code where sui.market_code="SH.3000005" and \
+                sms.circular_market_val != 0 order by \
+                sms.circular_market_val DESC')
+        if len(tmv) <= 0:
+            return -1
+        else:
+            return tmv
+
+    def get_partition_by_market_value(self, partitionN):
+        """返回分割成N份的上海和深圳的股票代码，按照市值降序
+        input: 分隔的份数
+        output: 如果失败返回-1，如果成功返回map，key是topN，value是股票代码tuple
+        """
+        if partitionN <= 0:
+            return -1
+        tmv = self.get_SHSZcode_order_by_market_value()
+        if tmv == -1:
+            return -1
+        stock_total_num = len(tmv)
+        if stock_total_num <= 0:
+            return -1
+        partition_step = math.floor(stock_total_num / partitionN)
+        if partition_step <= 1:
+            return -1
+        code_dict = {}
+        for one in range(1, partitionN+1):
+            key = "top" + str(one)
+            start_point = partition_step * (one - 1)
+            end_point = partition_step * one
+            code_dict[key] = tmv[start_point:end_point]
+        return code_dict
+
+    def get_close_price_by_stock_code(self, code, date):
+        """返回指定code代码+指定日期的收盘价
+        input: 股票代码，日期
+        output: 如果失败返回-1，如果成功返回该股票收盘价
+        """
+        run_sql="select nclose from stock_day_kline where \
+                time_key=\'" + date + "\' and code=\'" + code + "\'"
+        result_sql=sorted(self.nmysql.Query(run_sql))
+        if len(result_sql) > 0:
+            return result_sql[0][0]
+        else:
+            return -1
+
+    def get_PE_by_stock_code(self, code, date):
+        """返回指定code代码+指定日期的市盈率
+        input: 股票代码，日期$
+        output: 如果失败返回-1，如果成功返回该股票市盈率
+        """
+        run_sql="select pe_ratio from stock_day_kline where \
+                time_key=\'" + date + "\' and code=\'" + code + "\'"
+        result_sql=sorted(self.nmysql.Query(run_sql))
+        if len(result_sql) > 0:
+            return result_sql[0][0]
+        else:
+            return -1
+    
+    def get_turnover_GMV_by_stock_code(self, code, date):
+        """返回指定code代码+指定日期的成交额
+        input: 股票代码，日期$
+        output: 如果失败返回-1，如果成功返回该股票成交额
+        """
+        run_sql="select turnover from stock_day_kline where \
+                time_key=\'" + date + "\' and code=\'" + code + "\'"
+        result_sql=sorted(self.nmysql.Query(run_sql))
+        if len(result_sql) > 0:
+            return result_sql[0][0]
+        else:
+            return -1 
+
+    def get_turnover_ratio_by_stock_code(self, code, date):
+        """返回指定code代码+指定日期的换手率
+        input: 股票代码，日期$
+        output: 如果失败返回-1，如果成功返回该股票换手率
+        """
+        run_sql="select turnover_rate from stock_day_kline where \
+                time_key=\'" + date + "\' and code=\'" + code + "\'"
+        result_sql=sorted(self.nmysql.Query(run_sql))
+        if len(result_sql) > 0:
+            return result_sql[0][0]
+        else:
+            return -1 
+
+    def get_volume_by_stock_code(self, code, date):
+        """返回指定code代码+指定日期的成交量
+        input: 股票代码，日期$
+        output: 如果失败返回-1，如果成功返回该股票成交量
+        """
+        run_sql="select volume from stock_day_kline where \
+                time_key=\'" + date + "\' and code=\'" + code + "\'"
+        result_sql=sorted(self.nmysql.Query(run_sql))
+        if len(result_sql) > 0:
+            return result_sql[0][0]
+        else:
+            return -1
+
+    def get_outstanding_shares_by_stock_code(self, code, date):
+        """返回指定code代码+指定日期的流通总股数
+        input: 股票代码，日期$
+        output: 如果失败返回-1，如果成功返回该股票流通总股数
+        """
+        run_sql="select volume,turnover_rate from stock_day_kline where \
+                time_key=\'" + date + "\' and code=\'" + code + "\'"
+        result_sql=sorted(self.nmysql.Query(run_sql))
+        if len(result_sql) > 0:
+            volume = result_sql[0][0]
+            turnover_ratio = result_sql[0][1]
+            if turnover_ratio <= 0:
+                return -1
+            else:
+                return int(volume * 100 / turnover_ratio)
+        else:
+            return -1
+
+
+    def cal_stock_profit_ratio(self, stock_tuple, start, end):
+        """指定开始和结束日期，股票序列的收益率
+        input: 股票代码序列，开始日期，结束日期$
+        output: 如果失败返回-1，如果成功返回该序列收益率，每只股票的都按照1元买入
+        """
+        total = 0
+        for one in stock_tuple:
+            run_sql="select nclose from stock_day_kline where time_key \
+                    between \'" + start + "\' and \'" + end + "\' and code=\'" + \
+                    one[0] + "\'"
+            result_sql=sorted(self.nmysql.Query(run_sql))
+            if len(result_sql) == 0:
+                continue
+            if result_sql[0][0] == 0:
+                continue
+            profit_one=(float(result_sql[-1][0]) - \
+                    float(result_sql[0][0])) / float(result_sql[0][0])
+            total += profit_one
+            #print("start=%s end=%s start_nclose=%s end_nclose=%s \
+            #        profit=%s total=%s" %(start, end, result_sql[0][0], \
+            #        result_sql[-1][0], profit_one, total))
+        profit_total_ratio = total / len(stock_tuple)
+        #print("profit_total_ratio=%s" %(profit_total_ratio))
+        return profit_total_ratio
+
+if __name__ == '__main__':
+    partitionN = 1
+    stock_test = StockBasicInfo()
+    shsz_stock_dict = {}
+    shsz_stock_dict['top1'] = (('SZ.000002', '2018-12-12', 1.0), ('SH.601857', '2018-12-19', 1.0))
+    shsz_stock_dict['top2'] = (('SZ.000001', '2018-12-12', 1.0), ('SH.601398', '2018-12-19', 1.0))
+    print(stock_test.cal_stock_profit_ratio(shsz_stock_dict['top1'], stock_test.start_date, stock_test.end_date))
+    print(stock_test.cal_stock_profit_ratio(shsz_stock_dict['top2'], stock_test.start_date, stock_test.end_date))
+    #shsz_stock_dict = stock_test.get_partition_by_market_value(partitionN)
+    #for one in range(1, partitionN+1):
+    #    index = 'top' + str(one)
+    #    profit_total_ratio = stock_test.cal_stock_profit_ratio( \
+    #            shsz_stock_dict[index], \
+    #            stock_test.start_date, stock_test.end_date)
+    #    print("%s profitratio in %s and %s: %f" %(index, stock_test.start_date, \
+    #                stock_test.end_date, profit_total_ratio))
+    #    print(shsz_stock_dict[index]) 
---
+++ stock_basic_info_test.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import time
+from stock_basic_info import StockBasicInfo
+
+class StockBasicInfoTestCase(unittest.TestCase):
+    """StockBasicInfo类的单元测试用例
+    """
+    def setUp(self):
+        self.stock_basic_info = StockBasicInfo()
+        self.MAX_CHECK_DAY = 10
+        self.SECONDS_ONE_DAY = 86400
+        self.stock_code_tuple = \
+            self.stock_basic_info.get_SHSZcode_order_by_market_value()
+
+    def tearDown(self):
+        self.stock_basic_info = None
+
+    def test_get_SHSZcode_order_by_market_value_len(self):
+        """股票市值的测试用例
+        测试股票市值的tuple返回的长度是否在[1000, 100000]之间
+        """
+        stock_code_tuple = self.stock_code_tuple
+        checker_len = True
+        #测试返回对象的长度
+        if stock_code_tuple == -1:
+            checker_len = False
+        else:
+            tuple_len = len(stock_code_tuple)
+            if tuple_len > 100000:
+                checker_len = False
+            elif tuple_len < 1000:
+                checker_len = False
+            else:
+                checker_len = True
+        self.assertTrue(checker_len)
+
+    def test_get_SHSZcode_order_by_market_value_exist(self):
+        """股票市值的测试用例
+        测试A股股票市值中是否有上证的中国工商银行SH.601398
+        测试A股股票市值中是否有深证的平安银行SZ.000001
+        """
+        #测试是否存在SH.601398和SZ.000001
+        stock_code_tuple = self.stock_code_tuple
+        checker_exist = True
+        counter = 0
+        for one in stock_code_tuple:
+            if 'SH.601398' in one:
+                counter += 1
+            if 'SZ.000001' in one:
+                counter += 1
+        if counter < 2:
+            checker_exist = False
+        self.assertTrue(checker_exist)
+
+    def test_get_SHSZcode_order_by_market_value_update(self):
+        """股票市值的测试用例
+        测试SH.601398和SZ.000001是否最后更新时间都在15日之内
+        """
+        #测试SH.601398和SZ.000001是否最后更新时间都在15日之内
+        stock_code_tuple = self.stock_code_tuple
+        checker_update = True
+        counter = 0
+        for one in stock_code_tuple:
+            if 'SH.601398' in one:
+                update_time = one[1]
+                get_time = time.mktime(time.strptime(update_time,"%Y-%m-%d %H:%M:%S"))
+                now_time = time.time()
+                if now_time - get_time > self.SECONDS_ONE_DAY * self.MAX_CHECK_DAY:
+                    continue
+                else:
+                    counter += 1
+            if 'SZ.000001' in one:
+                update_time = one[1]
+                get_time = time.mktime(time.strptime(update_time,"%Y-%m-%d %H:%M:%S"))
+                now_time = time.time()
+                if now_time - get_time > self.SECONDS_ONE_DAY * self.MAX_CHECK_DAY:
+                    continue
+                else:
+                    counter += 1
+        if counter < 2:
+            checker_update = False
+        self.assertTrue(checker_update)
+
+    def test_get_partition_by_market_value(self):
+        """按市值排序分隔股票测试
+        测试当分隔份数为0时的返回，是否等于-1
+        测试当分隔份数为-1时的返回，是否等于-1
+        测试当分隔份数为10时的返回，是否等于len(self.stock_code_tuple)/10 or +1
+        """
+        checker_partition = True
+        error_msg = "NULL"
+        stock_partition = self.stock_basic_info.get_partition_by_market_value(\
+                0)
+        if stock_partition == -1:
+            pass
+        else:
+            checker_partition = False
+            error_msg = "partitionN=0, get no -1"
+        stock_partition = self.stock_basic_info.get_partition_by_market_value(\
+                -1)
+        if stock_partition == -1:
+            pass
+        else:
+            checker_partition = False
+            error_msg = "partitionN=-1, get no -1"
+        stock_partition = self.stock_basic_info.get_partition_by_market_value(\
+                10)
+        if stock_partition == -1:
+            checker_partition = False
+            error_msg = "partitionN=10, get -1"
+        else:
+            if len(stock_partition['top1']) < len(self.stock_code_tuple)/10 or \
+               len(stock_partition['top1']) >len(self.stock_code_tuple)/10 + 1:  
+                checker_partition = False
+                error_msg = "partitionN=10, get length " + \
+                str(len(stock_partition['top1']))
+            else:
+                pass
+        self.assertTrue(checker_partition, error_msg)
+
+    def test_cal_stock_profit_ratio(self):
+        """测试计算股票集收益
+        测试输入万科SZ.000002+SH.601857，时间段为2013-12-20~2018-12-20结果
+        """
+        shsz_stock_dict = {}
+        shsz_stock_dict['top1'] = (('SZ.000002', '2018-12-12', 1.0), \
+                                   ('SH.601857', '2018-12-19', 1.0))
+        start_date = '2013-12-20'
+        end_date = '2018-12-20'
+        checker_profit = True
+        expect_profit = 6.25
+        error_msg = "ok"
+        profit = self.stock_basic_info.cal_stock_profit_ratio(\
+                 shsz_stock_dict['top1'], start_date, end_date)
+        profit = round(profit, 2)
+        if profit != expect_profit:
+            checker_profit = False
+            error_msg = "get SZ.000002+SH.601857，时间段为2013-12-20~2018-12-20 \
+                        not 6.249103869810586"
+        self.assertTrue(checker_profit, error_msg)
+
+if __name__ == "__main__":
+    # 构造测试集
+    suite = unittest.TestSuite()
+    suite.addTest(StockBasicInfoTestCase( \
+                "test_get_SHSZcode_order_by_market_value_len"))
+    suite.addTest(StockBasicInfoTestCase( \
+                "test_get_SHSZcode_order_by_market_value_exist"))
+    suite.addTest(StockBasicInfoTestCase( \
+                "test_get_SHSZcode_order_by_market_value_update"))
+    suite.addTest(StockBasicInfoTestCase( \
+                "test_get_partition_by_market_value"))
+    suite.addTest(StockBasicInfoTestCase( \
+                "test_cal_stock_profit_ratio"))
+    
+    # 执行测试
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
